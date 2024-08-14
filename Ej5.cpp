215ae40a28e39979253c25383813614d14934d8d
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-08-14 17:44:47 -0300

    falta hacer

--- Ej5.cpp
+++ Ej5.cpp
@@ -1,3 +1,8 @@
+/*Ejercicio N°5
+Crear un programa que pida al usuario un número entero positivo "n" y un dígito
+"d". Luego crear una función recursiva que determine si “d” está en “n”. Si está, la
+función debe retornar verdadero y si no está debe retornar falso.
+Ej.: si n=1323 y d = 5, retorna Falso y si d= 1 retorna verdadero.*/
 #include <iostream>
 using namespace std;
 
